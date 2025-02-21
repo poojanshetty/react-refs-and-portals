5ed9624564c8571acc715d984237ada82b7a15e8
Pooja Shetty <poojaniranjanshetty@gmail.com>
2025-02-21 18:51:12 +0530

    Use portal to teleport the component to a specific elementbyid

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
   const dialog = useRef();
@@ -15,7 +16,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
     }
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost :D</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -28,6 +29,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   )
 }
